6ffaa3157a0519295dd6fc84e3adafa222ec102c
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-07-01 22:56:28 -0400

    Updates

---
+++ precache-manifest.f7bcfb9ffd94404d45e1f425bbd6c1b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a84fe4f8ea87b2bd7036d6bb02a7d9a",
+    "url": "/2022-Year-In-Review/index.html"
+  },
+  {
+    "revision": "69578dc4bbb700c25f8e",
+    "url": "/2022-Year-In-Review/static/css/2.fa593948.chunk.css"
+  },
+  {
+    "revision": "69578dc4bbb700c25f8e",
+    "url": "/2022-Year-In-Review/static/js/2.21d10f6f.chunk.js"
+  },
+  {
+    "revision": "4714554c87c8aacf3772",
+    "url": "/2022-Year-In-Review/static/js/main.36f054d9.chunk.js"
+  },
+  {
+    "revision": "8b922b991de5e375f8d1",
+    "url": "/2022-Year-In-Review/static/js/runtime~main.0d03fc7f.js"
+  },
+  {
+    "revision": "805108749829c1ab61090c392584be7c",
+    "url": "/2022-Year-In-Review/static/media/emptyCircleWhite.80510874.svg"
+  },
+  {
+    "revision": "8ab9540bf6d3ef24faba15fb1e8ce0db",
+    "url": "/2022-Year-In-Review/static/media/filledCircleWhite.8ab9540b.svg"
+  },
+  {
+    "revision": "522e0f1d669635eb1bb126a918c8d110",
+    "url": "/2022-Year-In-Review/static/media/readmore.522e0f1d.svg"
+  },
+  {
+    "revision": "45f31e796d84677235aff11bc0c85eca",
+    "url": "/2022-Year-In-Review/static/media/right_arrow.45f31e79.svg"
+  },
+  {
+    "revision": "d247d4a9e5d7880ad767cabc8fec42a4",
+    "url": "/2022-Year-In-Review/static/media/whitemasthead.d247d4a9.png"
+  },
+  {
+    "revision": "681a44b1b1d9341909fe0db6fc1ce980",
+    "url": "/2022-Year-In-Review/static/media/x.681a44b1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35ac214b8f1590908237e7bdd79ef3bc.js"
+  "/2022-Year-In-Review/precache-manifest.f7bcfb9ffd94404d45e1f425bbd6c1b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2022-Year-In-Review/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
